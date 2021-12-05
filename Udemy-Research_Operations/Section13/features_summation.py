1059836a29607d040388a49588fc11ef9c1af508
Juan <juan.armaguedon@icloud.com>
2021-12-05 12:57:50 +0100

    advanced features in pyomo

---
+++ Udemy-Research_Operations/Section13/features_summation.py
@@ -0,0 +1,42 @@
+import pyomo.environ as pyo
+from pyomo.opt import SolverFactory
+
+model = pyo.ConcreteModel()
+T = 10
+M = 4
+
+#variables
+model.x = pyo.Var(range(1,M+1), range(1,T+1), within=pyo.Integers)
+x = model.x
+
+#obj function
+model.obj = pyo.Objective(expr = pyo.summation(x), sense=pyo.maximize)# it is possible sum with a function provided by pyomo
+
+#constraints
+model.C1 = pyo.ConstraintList()
+for t in range(1,T+1):
+    model.C1.add(expr = 2*x[2,t] - 8*x[3,t] <= 0)
+    
+model.C2 = pyo.ConstraintList()
+for t in range(3,T+1):
+    model.C2.add(expr = x[2,t] - 2*x[3,t-2] + x[4,t] >= 1)
+    
+model.C3 = pyo.ConstraintList()
+for t in range(1,T+1):
+    model.C3.add(expr = sum([x[m,t] for m in range(1,M+1)]) <= 50)
+
+model.C4 = pyo.ConstraintList()
+for t in range(2,T+1):
+    model.C4.add(expr = x[1,t] + x[2,t-1] + x[3,t] + x[4,t] <= 10)
+
+model.C5 = pyo.ConstraintList()
+for m in range(1,M+1):
+    for t in range(1,T+1):
+        model.C5.add(pyo.inequality(0, x[m,t], 10))
+
+#solve
+opt = SolverFactory('glpk',executable='/usr/local/bin/glpsol')
+opt.options["mipgap"] = 0
+results = opt.solve(model, tee=True, timelimit = 10)
+
+print(pyo.value(model.obj))
